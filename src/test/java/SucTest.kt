cc56abc4fb63a617f726af5234e505470f857965
zhambyl <e.zhambul@gmail.com>
2016-11-23 00:50:03 +0300

    test

---
+++ src/test/java/SucTest.kt
@@ -0,0 +1,20 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Created by zhambyl on 23/11/2016.
+ */
+class SucTest {
+
+    @Test
+    fun test() {
+        val a = 2 * 2
+        assertEquals(a, 4)
+    }
+
+    @Test
+    fun failTest() {
+        val a = 2 * 2
+        assertEquals(a, 5)
+    }
+}
