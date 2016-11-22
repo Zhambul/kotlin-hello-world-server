032cf467da1956ecdcbbdf4dfaef267598047778
zhambyl <e.zhambul@gmail.com>
2016-11-23 00:27:04 +0300

    init commit

---
+++ src/main/java/Main.kt
@@ -0,0 +1,21 @@
+import io.vertx.core.Vertx
+
+/**
+ * Created by zhambyl on 23/11/2016.
+ */
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("starting server")
+        val server = Vertx.vertx().createHttpServer()
+
+        server.requestHandler {
+            val resp = it.response()
+            resp.putHeader("content-type", "text/plain");
+            resp.end("Hello, World!")
+        }
+
+        server.listen(8081)
+    }
+}
